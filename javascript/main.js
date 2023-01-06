5c2801493b6aac3d6f4eb2ff52675b4827494c4c
Valentino Grassi <valengrassi7@gmail.com>
2023-01-06 15:12:24 -0300

    5/1/2022

--- javascript/main.js
+++ javascript/main.js
@@ -5,6 +5,94 @@ const divEmpresas = document.querySelectorAll(".empresas-container")
 const servicios = document.querySelectorAll(".service")
 const contacts = document.querySelectorAll(".contacts")
 
+const texto = document.getElementById("leerMas")
+const flechas = document.querySelectorAll(".js")
+const divFotos = document.querySelectorAll(".divFotos")
+
+const servTec = document.getElementById("servTec")
+const programacion = document.getElementById("programacion")
+const cctv = document.getElementById("cctv")
+const telefonia = document.getElementById("telefonia")
+const antenas = document.getElementById("antenas")
+const estructura = document.getElementById("estructura")
+
+window.addEventListener("scroll", () => {
+    const scrolled = window.scrollY;
+    if (scrolled > 1) {
+        flechas.forEach(flecha => {
+            flecha.classList.remove("flecha")
+        })
+    }
+})
+
+servTec.addEventListener("click", () => {
+    Swal.fire({
+        showConfirmButton: false,
+        showCloseButton: true,
+        html:
+        '<i class="bi bi-pc-display" style="font-size: 50px; color: black;"></i>' + 
+        '<h3 class="tituloService" style="margin-top: 20px; color: black; font-size: 1.8rem;">Soporte técnico</h3>' +
+        '<p class="textoService" style="margin-top: 10px; font-size: 1.2rem; color: black;">Asesorías, proyecto básico, proyecto ejecutivo, asistencia técnica a la implementación, y otros servicios.</p>',
+    })
+})
+
+programacion.addEventListener("click", () => {
+    Swal.fire({
+        showConfirmButton: false,
+        showCloseButton: true,
+        html:
+        '<i class="bi bi-code-square" style="font-size: 50px; color: black;"></i>' + 
+        '<h3 class="tituloService" style="color: black; font-size: 1.8rem; margin-top: 10px;">Programación</h3>' +
+        '<p class="textoService" style=" font-size: 1.2rem; color: black; margin-top: 20px;">Páginas web para PC, tabletas y celulares, desde páginas informativas hasta tiendas online. Diseño y mantenimiento de las mismas al mejor precio.</p>',
+    })
+})
+
+cctv.addEventListener("click", () => {
+    Swal.fire({
+        showConfirmButton: false,
+        showCloseButton: true,
+        html:
+        '<i class="bi bi-camera-video" style="font-size: 50px; color: black;"></i>' + 
+        '<h3 class="tituloService" style="margin-top: 20px; color: black; font-size: 2rem;">CCTV</h3>' +
+        '<p class="textoService" style="margin-top: 10px; font-size: 1.2rem; color: black;">Sistemas Especiales, Tecnología digital Cámaras IP, fijas, domo, ocultas, exteriores, interiores, Sistemas de ilunminación IR y tecnologia LED, Gabración de video XVR, NVR.</p>',
+    })
+})
+
+telefonia.addEventListener("click", () => {
+    Swal.fire({
+        showConfirmButton: false,
+        showCloseButton: true,
+        html:
+        '<i class="bi bi-telephone" style="font-size: 50px; color: black;"></i>' + 
+        '<h3 class="tituloService" style="margin-top: 20px; color: black; font-size: 2rem;">Telefonía IP</h3>' +
+        '<p class="textoService" style="margin-top: 10px; font-size: 1.2rem; color: black;">Asesoramiento, implementación, customización, configuración, diseño de soluciones a medida, adaptación y reutilización de equipamiento, provisión de Hardware y mantenimiento. Desarrollo sobre la plataforma que brinda ASTERISK.</p>',
+    })
+})
+
+antenas.addEventListener("click", () => {
+    Swal.fire({
+        showConfirmButton: false,
+        showCloseButton: true,
+        html:
+        '<i class="bi bi-broadcast-pin" style="font-size: 50px; color: black;"></i>' + 
+        '<h3 class="tituloService" style="margin-top: 20px; color: black; font-size: 2rem;">Antenas satelitales</h3>' +
+        '<p class="textoService" style="margin-top: 10px; font-size: 1.2rem; color: black;">Asesoramiento, implementación, customización, configuración, diseño de soluciones a medida, adaptación y reutilización de equipamiento, provisión de Hardware y mantenimiento. Desarrollo sobre la plataforma que brinda ASTERISK.</p>',
+    })
+})
+
+estructura.addEventListener("click", () => {
+    Swal.fire({
+        width: "50vw",
+        showConfirmButton: false,
+        showCloseButton: true,
+        html:
+        '<i class="bi-building-fill" style="font-size: 50px; color: black;"></i>' + 
+        '<h3 class="tituloService" style="margin-top: 20px; color: black; font-size: 2rem;">Soluciones de infraestructura</h3>' +
+        '<p class="textoService" style="margin-top: 10px; font-size: 1.2rem; color: black;">Ofrecemos asesoramiento, diseño e implementación de enlaces y redes inalámbricas a la medida de sus necesidades, abarcando frecuencias en bandas licenciadas como no licenciadas. Ideales para redes de telemetría, datos y vigilancia que son ampliamente utilizados en redes de transmisión eléctrica, parques eólicos y yacimientos de gas y petróleo. Diseño y cálculo de redes de fibra óptica GPON-FTTH-PTP, Tendidos aéreos y subterráneos, Implementación end to end de redes ópticas, Gerenciamiento de implementaciones, Obra civil, Tuneleria inteligente, Análisis de espectro – Ocupación de bandas y frecuencias, Detección de fallas, Gestión de co-ubicación para equipamiento</p>',
+    })
+})
+
+
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting){
@@ -28,6 +116,8 @@ empresas.forEach(img => {
 servicios.forEach((servicio) => observer.observe(servicio))
 divEmpresas.forEach((empresa) => observer.observe(empresa))
 contacts.forEach((contact) => observer.observe(contact))
+divFotos.forEach((foto) => observer.observe(foto))
+
 
 $form.addEventListener("submit", handleSubmit)
 
